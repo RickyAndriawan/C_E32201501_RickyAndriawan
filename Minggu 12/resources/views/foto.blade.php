75db511ef57818db8635e50a211778d59db6140c
ricky <rickyandriawan22@gmail.com>
2021-06-10 23:47:19 +0700

    minggu ke 12

---
+++ Minggu 12/resources/views/foto.blade.php
@@ -0,0 +1,16 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Foto</title>
+ </head>
+ <body bgcolor= #00ffdd>
+ 	<center>
+ 		<font color="red" style="border:10px">
+ 			<h1>
+ 			INI FOTO SAYA
+ 			</h1>
+ 		</font>
+ 		<img src="{{ ('img/1.jpg') }}" width="60%" border="10px" style="color: #555555">
+ 	</center>
+ </body>
+ </html>
